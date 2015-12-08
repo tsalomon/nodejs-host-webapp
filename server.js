7f95161c9a0f618c05252c8b5f271ac1d6b23593
Tim Salomonsson <tsalomon@users.noreply.github.com>
2015-12-08 15:27:58 -0800

    added directory for serving gcal_publisher assets

--- server.js
+++ server.js
@@ -6,22 +6,27 @@ var path = require('path');
 // set the public folder to serve public assets
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/public/website1'));
+app.use(express.static(__dirname + '/public/gcal_publisher'));
 
 // setup alt route for website
 app.get('/site', function(req, res) {
 	res.sendFile(path.join(__dirname + '/public/website1/index.html'));
 });
-// route for gcal publisher app
+
+// routes for gcal publisher app
 app.get('/gapi', function(req, res) {
+	res.sendFile(path.join(__dirname + '/public/gcal_publisher/gapi.html'));
+});
+app.get('/gpub', function(req, res) {
 	res.sendFile(path.join(__dirname + '/public/gcal_publisher/gapi.html'));
 });
 
 //default to the website
-/*
+
 app.get('*', function(req, res) {
 	res.sendFile(path.join(__dirname + '/public/website1/index.html'));
 });
-*/
+
 
 
 // start the server on port 8080 (http://localhost:8080)
